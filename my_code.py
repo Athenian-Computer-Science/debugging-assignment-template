76866467fdaba50ab598ecad10df7368c585e854
Megan Leich <51799170+maleich@users.noreply.github.com>
2021-05-29 17:07:01 -0700

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -1,16 +1,11 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#
 
-def inc(x):
-    # User code goes here
-    return x + 1
+#------------------------------------------#
+# Find the errors and fix them.
+# Be sure the output matches the expected output.
+# Test your program before you submit it!
+#------------------------------------------#
 
-
-if __name__ == '__main__':
-    # Test your code with this first
-    # Change the argument to try different values
-    print(inc(6))
-
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #v = input("Value to increment: ")
-    #print(inc(int(v)))
+Input("What math class are you taking?') = math_class
+Print(f I love [math_class]!) 
