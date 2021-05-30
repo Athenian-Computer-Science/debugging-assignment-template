f446e88b07564f3eafa26c6941357d0f2a1ef854
mleich <mleich@athenian.org>
2021-05-29 19:38:13 -0700

    ??

--- my_code.py
+++ my_code.py
@@ -7,5 +7,8 @@
 # Test your program before you submit it!
 #------------------------------------------#
 
-Input("What math class are you taking?') = math_class
-Print(f I love [math_class]!) 
+#Input("What math class are you taking?') = math_class
+#Print(f I love [math_class]!)
+math_class = input("What math class are you taking? ")
+print(f'I love {math_class}!')
+
